9cbb9f897274c42d3414735c3b6d7021e9b2e373
DamirLapeiko <lapeiko02@mail.ru>
2022-12-23 19:27:45 +0300

    Lesson_3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите номер месяца");
+        int input = sc.nextInt();
+        switch (input){
+            case 1:
+                System.out.println("Зима");
+                break;
+            case 2:
+                System.out.println("Весна");
+                break;
+            case 3:
+                System.out.println("Лето");
+                break;
+            case 4:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Неверное число");
+        }
     }
 }
