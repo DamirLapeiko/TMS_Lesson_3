86e595b5a271d25efb45bf69ad9dac7d9344635a
DamirLapeiko <lapeiko02@mail.ru>
2022-12-28 19:17:33 +0300

    Homework

--- src/HW_Additional/Task3_A.java
+++ src/HW_Additional/Task3_A.java
@@ -1,37 +1,12 @@
 package HW_Additional;
 
-import java.io.Console;
-
 public class Task3_A {
     public static void main(String[] args) {
         for (int i = 1; i <= 9; i++) {
-                System.out.print("1*" + i + "=" + 1 * i + ' ');
-            } System.out.println();
-        for (int i = 1; i <= 9; i++) {
-            System.out.print("2*" + i + "=" + 2 * i + ' ');
-        } System.out.println();
-        for (int i = 1; i <= 9; i++) {
-            System.out.print("3*" + i + "=" + 3 * i + ' ');
-        } System.out.println();
-        for (int i = 1; i <= 9; i++) {
-            System.out.print("4*" + i + "=" + 4 * i + ' ');
-        } System.out.println();
-        for (int i = 1; i <= 9; i++) {
-            System.out.print("5*" + i + "=" + 5 * i + ' ');
-        } System.out.println();
-        for (int i = 1; i <= 9; i++) {
-            System.out.print("6*" + i + "=" + 6 * i + ' ');
-        } System.out.println();
-        for (int i = 1; i <= 9; i++) {
-            System.out.print("7*" + i + "=" + 7 * i + ' ');
-        } System.out.println();
-        for (int i = 1; i <= 9; i++) {
-            System.out.print("8*" + i + "=" + 8 * i + ' ');
-        } System.out.println();
-        for (int i = 1; i <= 9; i++) {
-            System.out.print("9*" + i + "=" + 9 * i + ' ');
-        } System.out.println();
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(i + " * " + j + " = " + i * j + "\t");
+            }
+            System.out.println();
         }
     }
-
-
+}
